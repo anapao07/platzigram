cb6261eea2c432bf00b34d375b17292bfac586d8
anapao07 <annapao.arcos@gmail.com>
2020-09-25 15:34:22 -0500

    Protecting the profile ,Detail View y List View

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,37 @@
+"""Posts views."""
+
+# Django
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
-# MOdels
-from posts.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
 # Forms
 from posts.forms import PostForm
 
+# Models
+from posts.models import Post
 
-@login_required
-def list_posts(request):
-    posts = Post.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
+
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created',)
+    paginate_by = 2
+    context_object_name = 'posts'
 
 
 @login_required
 def create_post(request):
-    """ CReate New Post View"""
+    """Create new post view."""
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('feed')
+            return redirect('posts:feed')
+
     else:
         form = PostForm()
 
